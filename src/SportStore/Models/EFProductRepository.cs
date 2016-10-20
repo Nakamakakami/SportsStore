3447cb98922df9c4265bd40183c2c5f765bf3293
Naka <Naka@DESKTOP-GM79T9T>
2016-10-20 06:34:31 +0200

    Create Database

---
+++ src/SportStore/Models/EFProductRepository.cs
@@ -0,0 +1,19 @@
+﻿using SportsStore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SportStore.Models
+{
+    public class EFProductRepository : IProductRepository
+    {
+        public ApplicationDbContext Context{ get; }
+        public EFProductRepository(ApplicationDbContext ctx)
+        {
+            Context = ctx;
+        }
+
+        public IEnumerable<Product> Products => Context.Products;
+    }
+}
