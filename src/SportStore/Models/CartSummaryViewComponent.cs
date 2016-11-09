948f9ac4687253e1cde5d8a0396875dab62d66a7
Naka <Naka@DESKTOP-GM79T9T>
2016-11-09 23:48:30 +0100

    Add order to project

---
+++ src/SportStore/Models/CartSummaryViewComponent.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SportsStore.Models
+{
+    public class CartSummaryViewComponent : ViewComponent
+    {
+        public Cart cart;
+        public CartSummaryViewComponent(Cart cartService)
+        {
+            cart = cartService;
+        }
+        public IViewComponentResult Invoke()
+        {
+            return View(cart);
+        }
+    }
+}
